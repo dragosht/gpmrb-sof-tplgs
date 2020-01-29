f565e6531f58b877e903717e58bfd1fb7d4ab976
Dragos Tarcatu <dragos_tarcatu@mentor.com>
2020-01-29 12:41:31 +0200

    scripts:
    Signed-off-by: Dragos Tarcatu <dragos_tarcatu@mentor.com>

--- compile.sh
+++ compile.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-alsatplg -v 1 -c sof-apl-tdf8532.conf -o sof-apl-tdf8532.tplg
-#alsatplg -v 1 -c sof-apl-nocodec.conf -o sof-apl-nocodec.tplg
+alsatplg -v 1 -c $1 -o $1.tplg
 
--- generate.sh
+++ generate.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
 m4 -I .. -I ../m4 -I ../common -I ../platform/common \
-	sof-apl-tdf8532.m4 > sof-apl-tdf8532.conf
-
-#m4 -I .. -I ../m4 -I ../common -I ../platform/common \
-#	sof-apl-nocodec.m4 > sof-apl-nocodec.conf
+	$1 > $1.conf
 
